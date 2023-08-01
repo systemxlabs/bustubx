1700cfa4fd883fa8c27e4f75d6e5d8b1e32aa6b9
Linwei Zhang <lewiszlw520@gmail.com>
2023-08-01 17:11:57 +0800

    Replace UnsafeCell with Inner solution

--- src/storage/disk_manager.rs
+++ src/storage/disk_manager.rs
@@ -1,8 +1,6 @@
 use std::{
-    cell::UnsafeCell,
     io::{Read, Seek, Write},
-    ops::{Deref, DerefMut},
-    sync::{atomic::AtomicU32, Arc, Mutex, RwLock},
+    sync::{atomic::AtomicU32, Mutex, MutexGuard},
 };
 
 use crate::common::config::TINYSQL_PAGE_SIZE;
@@ -12,10 +10,15 @@ use super::page::PageId;
 #[derive(Debug)]
 pub struct DiskManager {
     pub db_path: String,
-    pub db_file: UnsafeCell<std::fs::File>,
     pub next_page_id: AtomicU32,
-    db_io_latch: Mutex<()>,
+    inner: Mutex<Inner>,
 }
+
+#[derive(Debug)]
+struct Inner {
+    db_file: std::fs::File,
+}
+
 impl DiskManager {
     pub fn new(db_path: String) -> Self {
         let db_file = std::fs::OpenOptions::new()
@@ -32,76 +35,55 @@ impl DiskManager {
         println!("Initialized disk_manager next_page_id: {}", next_page_id);
         Self {
             db_path,
-            db_file: UnsafeCell::new(db_file),
             next_page_id: AtomicU32::new(next_page_id),
-            db_io_latch: Mutex::new(()),
+            inner: Mutex::new(Inner { db_file }),
         }
     }
 
     // 读取磁盘指定页的数据
     pub fn read_page(&self, page_id: PageId) -> [u8; TINYSQL_PAGE_SIZE] {
-        let lock = self.db_io_latch.lock().unwrap();
+        let mut guard = self.inner.lock().unwrap();
         let mut buf = [0; TINYSQL_PAGE_SIZE];
-        unsafe {
-            (*self.db_file.get())
-                .seek(std::io::SeekFrom::Start(
-                    (page_id as usize * TINYSQL_PAGE_SIZE) as u64,
-                ))
-                .unwrap();
-            (*self.db_file.get()).read_exact(&mut buf).unwrap();
-        }
-        drop(lock);
+        guard
+            .db_file
+            .seek(std::io::SeekFrom::Start(
+                (page_id as usize * TINYSQL_PAGE_SIZE) as u64,
+            ))
+            .unwrap();
+        guard.db_file.read_exact(&mut buf).unwrap();
         buf
     }
 
     // 将数据写入磁盘指定页
     pub fn write_page(&self, page_id: PageId, data: &[u8]) {
-        let lock = self.db_io_latch.lock().unwrap();
-        unsafe {
-            (*self.db_file.get())
-                .seek(std::io::SeekFrom::Start(
-                    (page_id as usize * TINYSQL_PAGE_SIZE) as u64,
-                ))
-                .unwrap();
-            (*self.db_file.get()).write_all(data).unwrap();
-        }
-        drop(lock);
+        let mut guard = self.inner.lock().unwrap();
+        Self::_write_page(&mut guard, page_id, data);
     }
 
     // TODO 使用bitmap管理
     pub fn allocate_page(&self) -> PageId {
-        let lock = self.db_io_latch.lock().unwrap();
+        let mut guard = self.inner.lock().unwrap();
         let page_id = self.next_page_id.load(std::sync::atomic::Ordering::SeqCst);
         self.next_page_id
             .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-        unsafe {
-            (*self.db_file.get())
-                .seek(std::io::SeekFrom::Start(
-                    (page_id as usize * TINYSQL_PAGE_SIZE) as u64,
-                ))
-                .unwrap();
-            (*self.db_file.get())
-                .write_all(&[0; TINYSQL_PAGE_SIZE])
-                .unwrap();
-        }
-        drop(lock);
+        Self::_write_page(&mut guard, page_id, &[0; TINYSQL_PAGE_SIZE]);
         page_id
     }
 
     pub fn deallocate_page(&self, page_id: PageId) {
         // TODO 利用pageId或者释放的空间
-        let lock = self.db_io_latch.lock().unwrap();
-        unsafe {
-            (*self.db_file.get())
-                .seek(std::io::SeekFrom::Start(
-                    (page_id as usize * TINYSQL_PAGE_SIZE) as u64,
-                ))
-                .unwrap();
-            (*self.db_file.get())
-                .write_all(&[0; TINYSQL_PAGE_SIZE])
-                .unwrap();
-        }
-        drop(lock);
+        let mut guard = self.inner.lock().unwrap();
+        Self::_write_page(&mut guard, page_id, &[0; TINYSQL_PAGE_SIZE]);
+    }
+
+    fn _write_page(guard: &mut MutexGuard<Inner>, page_id: PageId, data: &[u8]) {
+        guard
+            .db_file
+            .seek(std::io::SeekFrom::Start(
+                (page_id as usize * TINYSQL_PAGE_SIZE) as u64,
+            ))
+            .unwrap();
+        guard.db_file.write_all(data).unwrap();
     }
 }
 
